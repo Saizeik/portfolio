a4704b24b6803b66b185122daaa434f1fd192acd
Nathan Walker <nswalker@gmail.com>
2022-01-05 11:42:15 -0800

    update2

--- components/Portfolio/content.js
+++ components/Portfolio/content.js
@@ -1,72 +1,34 @@
-import netflix from '/public/netflix.jpg'
-import portfolioV1 from '/public/react-bootstrap.jpg'
-import reactMUIportfolio from '/public/react-mui-portfolio.jpg'
-import taskManager from '/public/task-manager.jpg'
-import mernBlog from '/public/mern-blog.jpg'
-import mercury from '/public/mercury.jpg'
-import cityGuide from '/public/city-guide.jpg'
-import nucampsite from '/public/nucampsite.jpg'
+import netflix from "/public/netflix.jpg";
 
-
+import nucampsite from "/public/nucampsite.jpg";
+import homeownify from "/public/Homeownify.jpg";
 
 export const content = [
   {
-    title: 'Netflix',
-    subtitle: 'Clone',
+    title: "Netflix",
+    subtitle: "Clone",
     Image: netflix,
-    src : '/public/netflix.jpg',
-    github: 'https://netflix-clone.git',
-    demo: 'https://netflix-clone.com',
+    src: "/public/netflix.jpg",
+    github: "https://netflix-clone.git",
+    demo: "https://netflix-clone.com",
   },
+
   {
-    title: 'React',
-    subtitle: 'Portfolio',
-    Image: portfolioV1,
-    github: 'https://react-portfolio.git',
-    demo: 'https://react-portfolio.com',
+    title: "HomeOwnify",
+    subtitle: "Own a Home",
+    Image: homeownify,
+    src: "/public/homeownify.jpg",
+    github: "https://github.com/Saizeik/Homeownify",
+    demo: "https://homeownify.netlify.app",
   },
+
   {
-    title: 'Task',
-    subtitle: 'Manager',
-    Image: taskManager,
-    github: 'https://task-manager.git',
-    demo: 'https://task-manager.com',
-  },
-  {
-    title: 'React',
-    subtitle: 'Material-UI',
-    Image: reactMUIportfolio,
-    github: 'https://react-material-ui.git',
-    demo: 'https://react-material-ui.com',
-  },
-  {
-    title: 'mern',
-    subtitle: 'blog',
-    Image: mernBlog,
-    github: 'https://mern-blog.git',
-    demo: 'https://mern-blog.com',
-  },
-  {
-    title: 'mercury',
-    subtitle: 'group',
-    Image: mercury,
-    github: 'https://mercury-group.git',
-    demo: 'https://mercury-group.com',
-  },
-  {
-    title: 'city',
-    subtitle: 'guide',
-    Image: cityGuide,
-    github: 'https://city-guide.git',
-    demo: 'https://city-guide.com',
-  },
-  {
-  title: 'Nucampsite',
-    subtitle: 'Nucamp Project',
+    title: "Nucampsite",
+    subtitle: "Nucamp Project",
     Image: nucampsite,
-    src : '/public/nucampsite.jpg',
-    
-    github: 'https://github.com/Saizeik/Nucampsite',
-    demo: 'https://nathanwalkernucamp.netlify.app',
-  }
-]
+    src: "/public/nucampsite.jpg",
+
+    github: "https://github.com/Saizeik/Nucampsite",
+    demo: "https://nathanwalkernucamp.netlify.app",
+  },
+];
