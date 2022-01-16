c5f55c2793f3c79fade19cf0d156b7e09ed99fa6
Nathan Walker <nswalker@gmail.com>
2022-01-15 16:45:37 -0800

    update

--- components/navbar/index.jsx
+++ components/navbar/index.jsx
@@ -1,91 +1,95 @@
-import React from 'react';
-import styles from './navbar.module.scss'
-import { Link } from 'react-scroll'
+import React from "react";
+import styles from "./navbar.module.scss";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
     <>
-
-      <nav className='navbar navbar-expand-lg navbar-light fixed-top'>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <button
-          className='navbar-toggler'
-          type='button'
-          data-toggle='collapse'
-          data-target='#navbarSupportedContent'
-          aria-controls='navbarSupportedContent'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
-          <span className='navbar-toggler-icon'></span>
+          <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div className='collapse navbar-collapse' id='navbarSupportedContent'>
-          
-        <ul className='navbar-nav m-auto'>
-          
-            <li className='nav-item active'>
-              <Link smooth={true} to='header' className=  {styles.navLink} href='#'><strong>
-                Home</strong> <span className='sr-only'>(current)</span>
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link
-                smooth={true}
-                to='about'
-                offset={-55}
-                className={styles.navLink}
-                href='#'
-              ><strong>
-                About Me</strong>
-              </Link>
-            </li>
-            
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav m-auto">
+              <li className="nav-item active">
+                <Link
+                  smooth={true}
+                  to="header"
+                  className={styles.navLink}
+                  href="#"
+                >
+                  <strong>Home</strong>{" "}
+                  <span className="sr-only">(current)</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  smooth={true}
+                  to="about"
+                  offset={-55}
+                  className={styles.navLink}
+                  href="#"
+                >
+                  <strong>About Me</strong>
+                </Link>
+              </li>
 
-            <li className='nav-item'>
-              <Link
-                smooth={true}
-                to='resume'
-                offset={-55}
-                className={styles.navLink}
-                href='#'
-              ><strong>
-                Resume</strong>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                smooth={true}
-                to='portfolio'
-                offset={-55}
-                className={styles.navLink}
-                href='#'
-              ><strong>
-                Portfolio</strong>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                smooth={true}
-                to='services'
-                offset={-55}
-                className={styles.navLink}
-                href='#'
-              ><strong>
-                Services</strong>
-              </Link>
-            </li>
-            <li className='nav-item'>
-              <Link
-                smooth={true}
-                to='contacts'
-                offset={-55}
-                className={styles.navLink}
-                href="#"
-              ><strong>
-                Contacts</strong>
-              </Link>
-            </li>
-          </ul>
+              <li className="nav-item">
+                <Link
+                  smooth={true}
+                  to="resume"
+                  offset={-55}
+                  className={styles.navLink}
+                  href="#"
+                >
+                  <strong>Resume</strong>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  smooth={true}
+                  to="portfolio"
+                  offset={-55}
+                  className={styles.navLink}
+                  href="#"
+                >
+                  <strong>Portfolio</strong>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  smooth={true}
+                  to="services"
+                  offset={-55}
+                  className={styles.navLink}
+                  href="#"
+                >
+                  <strong>Services</strong>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  smooth={true}
+                  to="contacts"
+                  offset={-55}
+                  className={styles.navLink}
+                  href="#"
+                >
+                  <strong>Contacts</strong>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
       <style>{`
@@ -123,9 +127,8 @@ const Navbar = () => {
   
   
   `}</style>
-      
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,9 @@
 import "../styles/globals.css";
-import "../styles/Home.module.css";
+
 import "/components/About/About.module.scss";
 
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
 import { config } from "@fortawesome/fontawesome-svg-core";
 config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
